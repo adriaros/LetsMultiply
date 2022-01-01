5032ef8833b7f784dc90539de7c328a99eb4a84e
AdriaRos <adria.ros@elma.care>
2022-01-01 20:57:53 +0100

    main
    - Init the app module

---
+++ LetsMultiply/LetsMultiply/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LetsMultiply
+//
+//  Created by Adrià Ros on 1/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LetsMultiply/LetsMultiply/LetsMultiplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsMultiplyApp.swift
+//  LetsMultiply
+//
+//  Created by Adrià Ros on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct LetsMultiplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
